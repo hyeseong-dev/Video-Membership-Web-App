097f3c6c0c300c62b52275e606358067975801d7
이혜성 <hyeseong43@gmail.com>
2021-12-26 20:47:02 +0900

    user create method and email validation

--- app/users/models.py
+++ app/users/models.py
@@ -3,6 +3,7 @@ from app.config import get_settings
 from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
 
+from . import validators
 
 settings = get_settings()
 
@@ -17,3 +18,16 @@ class User(Model):
     
     def __repr__(self):
         return f'User(email={self.email}, user_id={self.user_id})'
+    
+    @staticmethod
+    def create_user(email, password=None):
+        q = User.objects.filter(email=email)
+        if q.count() != 0:
+            raise Exception("User already has account.")
+        valid, msg, email = validators._validate_email(email=email)
+        if not valid:
+            raise Exception(f'Invalid email: {msg}')
+        obj = User(email=email)
+        obj.password = password
+        obj.save()
+        return obj
