d29f272121e8277bcc44d83f10e49eaabcc18284
hyeseong-lee <hyeseong43@gmail.com>
2021-12-30 19:25:44 +0900

    Create a User via the Python Shell

--- app/main.py
+++ app/main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
 
-from . import config
+from cassandra.cqlengine.management import sync_table
+
+from . import config, db
+from .users.models import User
 
 app = FastAPI()
 # settings = config.get_settings()
+DB_SESSION = None
 
 
+@app.on_event("startup")
+def on_startup():
+    # triggered when fastapi start
+    print("hello world")
+    global DB_SESSION
+    DB_SESSION = db.get_session()
+    sync_table(User)
 
 
-@app.get('/')
+@app.get("/")
 def homepage():
-    return dict(hello='world',)
+    return dict(
+        hello="world",
+    )
